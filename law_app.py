ad890fb318c8ca1fc683df07edfabef2e0cb7d89
Kavyaleodev <leodevkavya01@gmail.com>
2024-10-19 13:28:01 +0530

    push project

--- law_app.py
+++ law_app.py
@@ -113,7 +113,7 @@ def index():
         return render_template("index.html", sections=sections, complaint=complaint)
     return render_template("index.html", sections=[], complaint="")
 
+
 if __name__ == "__main__":
-    # Bind to 0.0.0.0 and use PORT from environment variable or default to 10000
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 10000)), debug=True)
+
